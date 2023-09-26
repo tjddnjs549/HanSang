18ee0f5b7688f47fac8ef48a003203d6393f7d19
seongwon <tjddnjs549@gmail.com>
2023-09-26 20:53:54 +0900

    [FEAT] : ⚙️ 재료 테이블뷰 구현

--- HanSang/View/DetailPage/DetailView.swift
+++ HanSang/View/DetailPage/DetailView.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 final class DetailView: UIView {
+
+    
     
     
     var isLiked: Bool = false
@@ -69,7 +71,7 @@ final class DetailView: UIView {
     private lazy var makeDifficultyLabel: UILabel = {
         let difficulty = UILabel()
         difficulty.labelMakeUI(textColor: UIColor.black, font: .systemFont(ofSize: 16))
-        difficulty.text = "초보중간"
+        difficulty.text = "초보"
         return difficulty
     }()
     
@@ -96,6 +98,29 @@ final class DetailView: UIView {
         return like
     }()
     
+    private let divider: UIView = {
+        let divider = UIView()
+        divider.backgroundColor = UIColor.lightGray
+        divider.translatesAutoresizingMaskIntoConstraints = false
+        return divider
+    }()
+    
+    private let materialLabel: UILabel = {
+        let label = UILabel()
+        label.labelMakeUI(textColor: UIColor.black, font: .boldSystemFont(ofSize: 20))
+        label.text = "레시피 재료"
+        return label
+    }()
+    
+    private lazy var materialTableView: UITableView = {
+        let table = UITableView()
+        table.delegate = self
+        table.dataSource = self
+        table.translatesAutoresizingMaskIntoConstraints = false
+        table.layer.borderWidth = 2.0
+        table.layer.borderColor = UIColor.black.cgColor
+        return table
+    }()
     
     // MARK: - init
 
@@ -115,7 +140,7 @@ final class DetailView: UIView {
         self.profileImageView.layer.borderColor = UIColor.black.cgColor
         self.profileImageView.clipsToBounds = true
         self.profileImageView.layer.cornerRadius = self.profileImageView.frame.width / 2
-
+        
     }
 
 }
@@ -128,6 +153,7 @@ private extension DetailView {
         scrollViewMakeUI()
         labelMakeUI()
         userInfoMakeUI()
+        materialMakeUI()
     }
     
     
@@ -150,17 +176,17 @@ private extension DetailView {
             contentView.topAnchor.constraint(equalTo: self.scrollView.topAnchor),
             contentView.bottomAnchor.constraint(equalTo: self.scrollView.bottomAnchor),
             
-            foodImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 10),
-            foodImageView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -10),
+            foodImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            foodImageView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
             foodImageView.topAnchor.constraint(equalTo: self.scrollView.topAnchor),
             foodImageView.heightAnchor.constraint(equalToConstant: 330),
             
-            titleLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant:  30),
-            titleLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -30),
-            titleLabel.topAnchor.constraint(equalTo: self.foodImageView.bottomAnchor, constant: 20)
+            titleLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant:  20),
+            titleLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -20),
+            titleLabel.topAnchor.constraint(equalTo: self.foodImageView.bottomAnchor, constant: 15)
         ])
     }
-    
+    //시간. 난이도 레이아웃
     func labelMakeUI() {
         
         self.contentView.addSubview(timeLabel)
@@ -169,8 +195,8 @@ private extension DetailView {
         self.contentView.addSubview(makeDifficultyLabel)
         
         NSLayoutConstraint.activate([
-            timeLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 30),
-            timeLabel.topAnchor.constraint(equalTo: self.titleLabel.bottomAnchor, constant: 20),
+            timeLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20),
+            timeLabel.topAnchor.constraint(equalTo: self.titleLabel.bottomAnchor, constant: 15),
             timeLabel.trailingAnchor.constraint(equalTo: self.makeTimeLabel.leadingAnchor, constant: -5),
             
             makeTimeLabel.topAnchor.constraint(equalTo: self.timeLabel.topAnchor),
@@ -180,21 +206,22 @@ private extension DetailView {
             difficultyLabel.trailingAnchor.constraint(equalTo: self.makeDifficultyLabel.leadingAnchor, constant:  -5),
             
             makeDifficultyLabel.topAnchor.constraint(equalTo: self.timeLabel.topAnchor),
-            makeDifficultyLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -30)
+            makeDifficultyLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -20)
         ])
     }
-    
+    //유저 정보 레이아웃
     func userInfoMakeUI() {
         
         self.contentView.addSubview(profileImageView)
         self.contentView.addSubview(profileNameLabel)
         self.contentView.addSubview(likeButton)
+        self.contentView.addSubview(divider)
         
         NSLayoutConstraint.activate([
             profileImageView.widthAnchor.constraint(equalToConstant: 50),
             profileImageView.heightAnchor.constraint(equalToConstant: 50),
-            profileImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 30),
-            profileImageView.topAnchor.constraint(equalTo: self.timeLabel.bottomAnchor, constant: 20),
+            profileImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20),
+            profileImageView.topAnchor.constraint(equalTo: self.timeLabel.bottomAnchor, constant: 15),
             profileImageView.trailingAnchor.constraint(equalTo: self.profileNameLabel.leadingAnchor, constant: -10),
             
             profileNameLabel.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 1/2),
@@ -204,10 +231,52 @@ private extension DetailView {
             likeButton.heightAnchor.constraint(equalToConstant: 20),
             likeButton.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -30),
             likeButton.centerYAnchor.constraint(equalTo: self.profileImageView.centerYAnchor),
+            
+            divider.heightAnchor.constraint(equalToConstant: 1.5),
+            divider.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 1),
+            divider.topAnchor.constraint(equalTo: self.profileImageView.bottomAnchor, constant: 15)
         ])
     }
     
+    //레시피 재료 레이아웃
+    func materialMakeUI() {
+        self.contentView.addSubview(materialLabel)
+        self.contentView.addSubview(materialTableView)
+        
+        NSLayoutConstraint.activate([
+            materialLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20),
+            materialLabel.topAnchor.constraint(equalTo: self.divider.bottomAnchor, constant: 20),
+            materialLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -50),
+        
+            materialTableView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20),
+            materialTableView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -30),
+            materialTableView.topAnchor.constraint(equalTo: self.materialLabel.bottomAnchor, constant: 20)
+        ])
+    }
 }
+
+
+// MARK: - table UITableViewDelegate / UITableViewDataSource
+
+
+extension DetailView: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
+
+
+extension DetailView: UITableViewDelegate {
+    
+}
+
+
+
+
 
 
 // MARK: - @objc func
@@ -217,7 +286,6 @@ extension DetailView {
     @objc func likeButtonTapped() {
         isLiked.toggle()
         likeButton.isSelected = isLiked // 버튼의 선택 상태를 업데이트
-        print("sd")
     }
 }
 
