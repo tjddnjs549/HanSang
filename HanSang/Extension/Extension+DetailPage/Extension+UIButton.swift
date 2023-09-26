64b41a13f7489d98a27b69b5a98f5e4716693e6f
seongwon <tjddnjs549@gmail.com>
2023-09-26 20:18:42 +0900

    [FEAT] : ⚙️  유저 정보까지 구현

---
+++ HanSang/Extension/Extension+DetailPage/Extension+UIButton.swift
@@ -0,0 +1,31 @@
+//
+//  Extension+UIButton.swift
+//  HanSang
+//
+//  Created by 박성원 on 2023/09/26.
+//
+
+import UIKit
+
+extension UIButton {
+    
+    func buttonMakeUI(cornerRadius: CGFloat, borderWidth: CGFloat, borderColor: CGColor, setTitle: String, font: UIFont, setTitleColor:  UIColor) {
+        self.backgroundColor = .clear
+        self.layer.cornerRadius = cornerRadius
+        self.clipsToBounds = true //둥글게
+        self.layer.borderWidth = borderWidth
+        self.layer.borderColor = borderColor
+        self.setTitle(setTitle, for: .normal)
+        self.setTitleColor(setTitleColor, for: .normal)
+        self.titleLabel?.font = font
+        self.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    func buttonImageMakeUI(image: String, selectedImage: String, color: UIColor) {
+        self.setImage(UIImage(systemName: "\(image)"), for: .normal) //선택 x
+        self.setImage(UIImage(systemName: "\(selectedImage)"), for: .selected) // 선택 o
+        self.tintColor = color // 아이콘 색상
+        self.translatesAutoresizingMaskIntoConstraints = false
+        self.isEnabled = true
+    }
+}
--- HanSang/View/DetailPage/DetailView.swift
+++ HanSang/View/DetailPage/DetailView.swift
@@ -9,6 +9,8 @@ import UIKit
 
 final class DetailView: UIView {
     
+    
+    var isLiked: Bool = false
     // MARK: - properties
 
     private let scrollView: UIScrollView = {
@@ -29,14 +31,14 @@ final class DetailView: UIView {
     
     private lazy var foodImageView: UIImageView = {
         let image = UIImageView()
-        image.image = UIImage(named: "food")
+        image.image = UIImage(named: "Food1")
         image.contentMode = .scaleAspectFit
         image.clipsToBounds = true
         image.translatesAutoresizingMaskIntoConstraints = false
         return image
     }()
     
-    lazy var titleLabel: UILabel = {
+    private lazy var titleLabel: UILabel = {
         let title = UILabel()
         title.labelMakeUI(textColor: UIColor.black, font: .systemFont(ofSize: 24))
         title.text = "집에서도 미디움레어가 가능한 스테이크 레시피"
@@ -50,7 +52,7 @@ final class DetailView: UIView {
         return time
     }()
     
-    lazy var makeTimeLabel: UILabel = {
+    private lazy var makeTimeLabel: UILabel = {
         let time = UILabel()
         time.labelMakeUI(textColor: UIColor.black, font: .systemFont(ofSize: 16))
         time.text = "1시간30분"
@@ -64,15 +66,36 @@ final class DetailView: UIView {
         return difficulty
     }()
     
-    lazy var makeDifficultyLabel: UILabel = {
+    private lazy var makeDifficultyLabel: UILabel = {
         let difficulty = UILabel()
         difficulty.labelMakeUI(textColor: UIColor.black, font: .systemFont(ofSize: 16))
         difficulty.text = "초보중간"
         return difficulty
     }()
     
+    private lazy var profileImageView: UIImageView = {
+        let image = UIImageView()
+        image.image = UIImage(named: "ProfileImage")
+        image.contentMode = .scaleAspectFit
+        image.clipsToBounds = true
+        image.translatesAutoresizingMaskIntoConstraints = false
+        return image
+    }()
     
-   
+    private lazy var profileNameLabel: UILabel = {
+        let label = UILabel()
+        label.labelMakeUI(textColor: UIColor.black, font: .boldSystemFont(ofSize: 16))
+        label.text = "닉네임입니다!"
+        return label
+    }()
+    
+    private lazy var likeButton: UIButton = {
+        let like = UIButton(type: .custom)
+        like.buttonImageMakeUI(image: "heart", selectedImage: "heart.fill", color: .red)
+        like.addTarget(self, action: #selector(likeButtonTapped), for: .touchUpInside)
+        return like
+    }()
+    
     
     // MARK: - init
 
@@ -85,6 +108,15 @@ final class DetailView: UIView {
         fatalError("init(coder:) has not been implemented")
     }
     
+    
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        self.profileImageView.layer.borderWidth = 1.0
+        self.profileImageView.layer.borderColor = UIColor.black.cgColor
+        self.profileImageView.clipsToBounds = true
+        self.profileImageView.layer.cornerRadius = self.profileImageView.frame.width / 2
+
+    }
 
 }
 
@@ -95,6 +127,7 @@ private extension DetailView {
     func configureUI() {
         scrollViewMakeUI()
         labelMakeUI()
+        userInfoMakeUI()
     }
     
     
@@ -140,20 +173,51 @@ private extension DetailView {
             timeLabel.topAnchor.constraint(equalTo: self.titleLabel.bottomAnchor, constant: 20),
             timeLabel.trailingAnchor.constraint(equalTo: self.makeTimeLabel.leadingAnchor, constant: -5),
             
-            makeTimeLabel.topAnchor.constraint(equalTo: self.titleLabel.bottomAnchor, constant: 20),
+            makeTimeLabel.topAnchor.constraint(equalTo: self.timeLabel.topAnchor),
             makeTimeLabel.trailingAnchor.constraint(equalTo: self.difficultyLabel.leadingAnchor, constant: -20),
             
-            difficultyLabel.topAnchor.constraint(equalTo: self.titleLabel.bottomAnchor, constant: 20),
+            difficultyLabel.topAnchor.constraint(equalTo: self.timeLabel.topAnchor),
             difficultyLabel.trailingAnchor.constraint(equalTo: self.makeDifficultyLabel.leadingAnchor, constant:  -5),
             
-            makeDifficultyLabel.topAnchor.constraint(equalTo: self.titleLabel.bottomAnchor, constant: 20),
+            makeDifficultyLabel.topAnchor.constraint(equalTo: self.timeLabel.topAnchor),
             makeDifficultyLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -30)
-        
         ])
     }
     
-    
+    func userInfoMakeUI() {
+        
+        self.contentView.addSubview(profileImageView)
+        self.contentView.addSubview(profileNameLabel)
+        self.contentView.addSubview(likeButton)
+        
+        NSLayoutConstraint.activate([
+            profileImageView.widthAnchor.constraint(equalToConstant: 50),
+            profileImageView.heightAnchor.constraint(equalToConstant: 50),
+            profileImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 30),
+            profileImageView.topAnchor.constraint(equalTo: self.timeLabel.bottomAnchor, constant: 20),
+            profileImageView.trailingAnchor.constraint(equalTo: self.profileNameLabel.leadingAnchor, constant: -10),
+            
+            profileNameLabel.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 1/2),
+            profileNameLabel.centerYAnchor.constraint(equalTo: self.profileImageView.centerYAnchor),
+            
+            likeButton.widthAnchor.constraint(equalToConstant: 20),
+            likeButton.heightAnchor.constraint(equalToConstant: 20),
+            likeButton.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -30),
+            likeButton.centerYAnchor.constraint(equalTo: self.profileImageView.centerYAnchor),
+        ])
+    }
     
 }
 
+
+// MARK: - @objc func
+
+extension DetailView {
+    
+    @objc func likeButtonTapped() {
+        isLiked.toggle()
+        likeButton.isSelected = isLiked // 버튼의 선택 상태를 업데이트
+        print("sd")
+    }
+}
 
