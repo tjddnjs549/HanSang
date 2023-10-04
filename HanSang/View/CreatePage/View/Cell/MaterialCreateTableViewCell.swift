5adcbc62e14de5eec2634cd9e32851b51d5ee6d7
Seoon KIM <6872065@gmail.com>
2023-10-04 17:40:13 +0900

    [FIX] : 🐛 textField Delegate 및 TimerView 추가

--- HanSang/View/CreatePage/View/Cell/MaterialCreateTableViewCell.swift
+++ HanSang/View/CreatePage/View/Cell/MaterialCreateTableViewCell.swift
@@ -8,7 +8,7 @@
 import UIKit
 import SnapKit
 
-class MaterialCreateTableViewCell: UITableViewCell {
+class MaterialCreateTableViewCell: UITableViewCell, UITextFieldDelegate {
     
     // MARK: - Properties
     
