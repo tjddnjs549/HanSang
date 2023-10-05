8a9f9fc4b87297d6750473d1f472ea3d73f44dda
YoungKunChoi <youn9kun@gmail.com>
2023-10-05 19:27:57 +0900

    [FEAT] : ⚙️  Searchpage 버튼수정

--- HanSang/View/MainPage/SearchViewController.swift
+++ HanSang/View/MainPage/SearchViewController.swift
@@ -20,7 +20,7 @@ class SearchViewController: UIViewController {
         
         let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
         collectionView.backgroundColor = .systemBackground
-        collectionView.register(CostomCollectionViewCell.self, forCellWithReuseIdentifier: CostomCollectionViewCell.identifier)
+        collectionView.register(MyPageCustomCell.self, forCellWithReuseIdentifier: MyPageCustomCell.identifier)
         return collectionView
     }()
     
@@ -39,8 +39,16 @@ class SearchViewController: UIViewController {
         self.navigationItem.hidesSearchBarWhenScrolling = false
         let searchbar = UISearchBar(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width - 10, height: 50))
         searchbar.sizeToFit()
-        
         searchbar.placeholder = "Search User"
+        // Create a custom back button item
+        let chevronLeft = UIImage(systemName: "chevron.left")
+        let customBackButton = UIBarButtonItem(image: chevronLeft, style: .plain, target: self, action: #selector(backButtonTapped))
+        customBackButton.tintColor = ColorGuide.textHint
+        cancel.tintColor = ColorGuide.textHint
+
+        // Set the custom back button as the left navigation bar button item
+        self.navigationItem.leftBarButtonItem = customBackButton
+
 
         for _ in 0...25 {
             images.append(UIImage(named: "1")!)
@@ -84,14 +92,13 @@ extension SearchViewController: UICollectionViewDataSource, UICollectionViewDele
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CostomCollectionViewCell.identifier, for: indexPath) as?
-                CostomCollectionViewCell else {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MyPageCustomCell.identifier, for: indexPath) as?
+                MyPageCustomCell else {
             fatalError("Failed to dequeue CostomCollectionViewCell in MainViewController")
         }
         
-        let image = self.images[indexPath.row]
-        cell.configure(with: image)
-        cell.configure(with: image)
+        let image = images[indexPath.row]
+        cell.configure(image)
         cell.layer.borderWidth = 1.0
         cell.layer.borderColor = ColorGuide.inputLine.cgColor
         cell.layer.cornerRadius = 12
@@ -105,36 +112,6 @@ extension SearchViewController: UICollectionViewDataSource, UICollectionViewDele
     }
 }
 
-//extension  SearchViewController: UICollectionViewDelegateFlowLayout {
-//
-//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-//        print(self.view.frame.width)
-//        let size = (self.view.frame.width-60)/2
-//        return CGSize(width: size, height: size)
-//    }
-//
-//    // Vertical Specing
-//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-//        20
-//    }
-//
-////    // Horizontal Specing
-////    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
-////        8
-////    }
-//
-////    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-////        return UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
-////    }
-//
-//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
-//        return CGSize(width: 200, height: 77)
-//    }
-//
-//
-//
-//}
-
 extension SearchViewController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         let size = (view.bounds.size.width - 76) / 2
@@ -146,6 +123,6 @@ extension SearchViewController: UICollectionViewDelegateFlowLayout {
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
-        return CGSize(width: 200, height: 77)
+        return CGSize(width: 200, height: 0)
     }
 }
